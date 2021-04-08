012d4c282c7793b8e55be0edb275826532da7ef1
thirdsgames <thirdsgames2018@gmail.com>
2021-04-08 18:38:27 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,44 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const { exec } = require("child_process");
+
+try {
+    const llvmVersion = core.getInput('llvm-version');
+    const env = core.getInput('env-var');
+    core.info(`Installing LLVM version ${nameToGreet}...`);
+
+    switch (process.platform) {
+        case "win32": {
+            throw "Windows not supported yet"
+            break;
+        }
+        case "darwin": {
+            throw "MacOS not supported yet"
+            break;
+        }
+        default: {
+            // Assume linux.
+            const installLocationRoot = "/home/runner/llvm";
+            switch (llvmVersion) {
+                case "11.1.0": {
+                    const installLocation = installLocationRoot + "/clang+llvm-11.1.0-x86_64-linux-gnu-ubuntu-16.04";
+                    exec("curl -LJO https://github.com/llvm/llvm-project/releases/download/llvmorg-11.1.0/clang+llvm-11.1.0-x86_64-linux-gnu-ubuntu-16.04.tar.xz");
+                    exec("tar -xvf clang+llvm-11.1.0-x86_64-linux-gnu-ubuntu-16.04.tar.xz -C " + installLocationRoot);
+                    exec("rm clang+llvm-11.1.0-x86_64-linux-gnu-ubuntu-16.04.tar.xz");
+                    exec(installLocation + "/bin/llvm-config --version");
+                    if (env != "") {
+                        core.exportVariable(env, installLocation);
+                    }
+                    break;
+                }
+                default: {
+                    throw `LLVM version ${llvmVersion} not supported on Linux`
+                }
+            }
+        }
+    }
+
+    core.info("Installation completed!");
+} catch (error) {
+    core.setFailed(error.message);
+}
