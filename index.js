a469757066933407f1ab3acb63021134afdf86e2
thirdsgames <thirdsgames2018@gmail.com>
2021-04-08 19:09:04 +0100

    Execute synchronously

--- index.js
+++ index.js
@@ -1,15 +1,9 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-const { exec } = require("child_process");
+const { execSync } = require("child_process");
 
-function ex(proc) {
-    var child = exec(proc);
-    child.stdout.on('data', function (data) {
-        core.info(data.toString());
-    });
-    child.stderr.on('data', function (data) {
-        core.info(data.toString());
-    });
+function exec(proc) {
+    execSync(proc, { stdio: 'inherit' });
 }
 
 try {
@@ -22,21 +16,21 @@ try {
             const installLocation = "C:/llvm";
             switch (llvmVersion) {
                 case "11.0.0": {
-                    ex("mkdir " + installLocation);
+                    exec("mkdir " + installLocation);
 
-                    ex("curl -LJO https://github.com/thirdsgames/llvm-binaries-win/releases/download/latest/llvm-11.0.0.bin.tar.gz");
-                    ex("tar -xvf llvm-11.0.0.bin.tar.gz -C " + installLocation);
-                    ex("rm llvm-11.0.0.bin.tar.gz");
+                    exec("curl -LJO https://github.com/thirdsgames/llvm-binaries-win/releases/download/latest/llvm-11.0.0.bin.tar.gz");
+                    exec("tar -xvf llvm-11.0.0.bin.tar.gz -C " + installLocation);
+                    exec("rm llvm-11.0.0.bin.tar.gz");
 
-                    ex("curl -LJO https://github.com/thirdsgames/llvm-binaries-win/releases/download/latest/llvm-11.0.0.lib.tar.gz");
-                    ex("tar -xvf llvm-11.0.0.lib.tar.gz -C " + installLocation);
-                    ex("rm llvm-11.0.0.lib.tar.gz");
+                    exec("curl -LJO https://github.com/thirdsgames/llvm-binaries-win/releases/download/latest/llvm-11.0.0.lib.tar.gz");
+                    exec("tar -xvf llvm-11.0.0.lib.tar.gz -C " + installLocation);
+                    exec("rm llvm-11.0.0.lib.tar.gz");
 
-                    ex("curl -LJO https://github.com/thirdsgames/llvm-binaries-win/releases/download/latest/llvm-11.0.0.include.tar.gz");
-                    ex("tar -xvf llvm-11.0.0.include.tar.gz -C " + installLocation);
-                    ex("rm llvm-11.0.0.include.tar.gz");
+                    exec("curl -LJO https://github.com/thirdsgames/llvm-binaries-win/releases/download/latest/llvm-11.0.0.include.tar.gz");
+                    exec("tar -xvf llvm-11.0.0.include.tar.gz -C " + installLocation);
+                    exec("rm llvm-11.0.0.include.tar.gz");
 
-                    ex(installLocation + "/bin/llvm-config.exe --version");
+                    exec(installLocation + "/bin/llvm-config.exe --version");
                     if (env != "") {
                         core.exportVariable(env, installLocation);
                     }
@@ -55,15 +49,15 @@ try {
         default: {
             // Assume linux.
             const installLocationRoot = "/home/runner/llvm";
-            ex("mkdir " + installLocationRoot);
+            exec("mkdir " + installLocationRoot);
             switch (llvmVersion) {
                 case "11.1.0": {
                     const installLocation = installLocationRoot + "/clang+llvm-11.1.0-x86_64-linux-gnu-ubuntu-16.04";
-                    ex("mkdir " + installLocation);
-                    ex("curl -LJO https://github.com/llvm/llvm-project/releases/download/llvmorg-11.1.0/clang+llvm-11.1.0-x86_64-linux-gnu-ubuntu-16.04.tar.xz");
-                    ex("tar -xvf clang+llvm-11.1.0-x86_64-linux-gnu-ubuntu-16.04.tar.xz -C " + installLocationRoot);
-                    ex("rm clang+llvm-11.1.0-x86_64-linux-gnu-ubuntu-16.04.tar.xz");
-                    ex(installLocation + "/bin/llvm-config --version");
+                    exec("mkdir " + installLocation);
+                    exec("curl -LJO https://github.com/llvm/llvm-project/releases/download/llvmorg-11.1.0/clang+llvm-11.1.0-x86_64-linux-gnu-ubuntu-16.04.tar.xz");
+                    exec("tar -xvf clang+llvm-11.1.0-x86_64-linux-gnu-ubuntu-16.04.tar.xz -C " + installLocationRoot);
+                    exec("rm clang+llvm-11.1.0-x86_64-linux-gnu-ubuntu-16.04.tar.xz");
+                    exec(installLocation + "/bin/llvm-config --version");
                     if (env != "") {
                         core.info(`Setting environment variable ${env} to ${installLocation}.`);
                         core.exportVariable(env, installLocation);
